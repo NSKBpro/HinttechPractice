336863753d3482186a6e84746bd947a94a829591
NSKBpro <nskb992@gmail.com>
2015-07-03 14:18:31 +0200

    Inicjalni

---
+++ HinttechPractice.Service/TestService.cs
@@ -0,0 +1,29 @@
+﻿using HinttechPractice.Data;
+using HinttechPractice.Models;
+
+namespace HinttechPractice.Service
+{
+    class TestService
+    {
+        private readonly hinttechPracticeEntities dataContext;
+
+        public TestService()
+        {
+            if (dataContext == null)
+            {
+                dataContext = new hinttechPracticeEntities();
+            }
+        }
+
+        public void Add(TestViewModel testModel)
+        {
+            var newUser = new User
+            {
+                Firstname = testModel.Name
+            };
+
+            dataContext.Users.Add(newUser);
+            dataContext.SaveChanges();
+        }
+    }
+}
